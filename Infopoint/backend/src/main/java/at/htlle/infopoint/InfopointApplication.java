f7258aa9c75a274d9c276cea96100b0147acde82
michi-haensler <211witb08@o365.htl-leoben.at>
2025-10-01 15:40:02 +0200

    Initialize Infopoint project with backend and frontend

---
+++ Infopoint/backend/src/main/java/at/htlle/infopoint/InfopointApplication.java
@@ -0,0 +1,13 @@
+package at.htlle.infopoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfopointApplication.class, args);
+	}
+
+}
---
+++ Infopoint/backend/src/test/java/at/htlle/infopoint/InfopointApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.infopoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfopointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
